8fcaee35ef9f2d9bfd41f24aaa88fabc58d331a2
nhatpha234 <125719371+nhatpha234@users.noreply.github.com>
2023-04-07 08:08:15 +0700

    Ví dụ Phương pháp sắp xếp đổi chỗ trực tiếp

---
+++ CTDL&GT_Interchange_Sort_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void Interchange_Sort(int a[], int n){
+	int i,j;
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]);//hoán vị a[i] và a[j]
+			}
+		}
+	}
+}
+//Ví dụ Phương pháp sắp xếp đổi chỗ trực tiếp: https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-sap-xep-doi-cho-truc-tiep-interchange-sort
